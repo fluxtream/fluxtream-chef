74af839b06ec734b4ab7e8987d3216ab47b5c905
Candide Kemmler <ckemmler@gmail.com>
2013-05-21 21:24:46 +0200

    updated scripts in order to build latest master branch

--- cookbooks/fluxtream_main/recipes/clone-fluxtream-app.rb
+++ cookbooks/fluxtream_main/recipes/clone-fluxtream-app.rb
@@ -9,13 +9,6 @@ git "clone fluxtream-app repo" do
 	action :sync
 end
 
-execute "checkout idempotent_updates" do
-  command "git checkout idempotent_updates"
-  cwd "/home/fluxtream/projects/fluxtream-app"
-  user "fluxtream"
-  action :run
-end
-
 execute "copy sample properties" do
   command "cp /home/fluxtream/projects/fluxtream-app/fluxtream-web/src/main/resources/samples/* /home/fluxtream/projects/fluxtream-app/fluxtream-web/src/main/resources"
   user "fluxtream"
